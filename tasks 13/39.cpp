c9ea186486a8f89dc70f8d6afff01990235f55b2
Vlad <44032620+d0m1nus@users.noreply.github.com>
2019-04-22 17:33:21 +0700

    Create 39.cpp

---
+++ tasks 13/39.cpp
@@ -0,0 +1,54 @@
+#include "pch.h"
+#include <iostream>
+#include <clocale>
+#include <cstring>
+#include <fstream>
+
+using namespace std;
+
+void wrap_text(ifstream &ist, ofstream &ost, int max_line_len) {
+	char line[255];
+	int k = 1;
+	while (ist.getline(line, 255)) {
+		int i = 0;
+		for (i; i < strlen(line); i++) {
+			if (i == max_line_len * k) {
+				ost << endl;
+				k++;
+			}
+			ost << line[i];
+		}
+		k = 1;
+		if (!ist.eof()) {
+			ost << endl;
+		}
+		
+	}
+}
+
+
+int main() {
+	setlocale(LC_ALL, "");
+
+	ifstream ist("in.txt");
+	ofstream ost("out.txt");
+
+	int a = 12, *ptr;
+	ptr = &a;
+
+	if (!ist.is_open()) {
+		cout << "Не удалось открыть файл" << endl;
+		return -1;
+	}
+
+	int max_line_len;
+	cout << "Введите значение max_line_len: ";
+	cin >> max_line_len;
+
+	wrap_text(ist, ost, max_line_len);
+
+	cout << "Проверьте файл!" << endl;
+	ist.close();
+	ost.close();
+	return 0;
+}
