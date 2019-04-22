e43ebaa4da22aee2dbfbf01c1e24fefe7fedd5f9
Vlad <44032620+d0m1nus@users.noreply.github.com>
2019-04-22 16:35:58 +0700

    Create 0.cpp

---
+++ practical work 1/0.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include <iostream>
+#include <clocale>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	printf("Hello wold!\n");
+	printf("Привет мир!\n");
+
+    cout << "Hello World!\n"; 
+	cout << "Привет мир!\n";
+
+	return 0;
+}
